cba2f8d32ced027b5837ceae5a6ffe256982c69e
arsi <arsi@arsi.sk>
2017-12-27 19:14:22 +0100

    #56 ADB Shell Action

--- src/main/java/org/nbandroid/netbeans/gradle/v2/Installer.java
+++ src/main/java/org/nbandroid/netbeans/gradle/v2/Installer.java
@@ -8,13 +8,23 @@ package org.nbandroid.netbeans.gradle.v2;
 import com.android.ddmlib.AndroidDebugBridge;
 import org.nbandroid.netbeans.gradle.core.ddm.AndroidDebugBridgeFactory;
 import org.nbandroid.netbeans.gradle.v2.sdk.AndroidSdkProvider;
+import org.netbeans.modules.dlight.terminal.ui.TerminalContainerTopComponent;
+import static org.netbeans.modules.dlight.terminal.ui.TerminalContainerTopComponent.AUTO_OPEN_LOCAL_PROPERTY;
 import org.openide.modules.ModuleInstall;
+import org.openide.windows.WindowManager;
 
 public class Installer extends ModuleInstall {
 
     @Override
     public void restored() {
         AndroidSdkProvider.getDefault(); //init SDK subsystem
+        Runnable runnable = new Runnable() {
+            public void run() {
+                TerminalContainerTopComponent instance = TerminalContainerTopComponent.findInstance();
+                instance.putClientProperty(AUTO_OPEN_LOCAL_PROPERTY, Boolean.FALSE);
+            }
+        };
+        WindowManager.getDefault().invokeWhenUIReady(runnable);
     }
 
     @Override
