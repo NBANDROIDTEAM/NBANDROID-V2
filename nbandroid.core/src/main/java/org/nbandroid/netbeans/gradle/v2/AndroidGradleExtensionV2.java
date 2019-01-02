4c84bb13702f8aaf604166fc33e4b9818432ac67
arsi-apli <arsi@arsi.sk>
2019-01-02 09:35:14 +0100

    resolved #154, #153 removed Guava Android patches

--- nbandroid.core/src/main/java/org/nbandroid/netbeans/gradle/v2/AndroidGradleExtensionV2.java
+++ nbandroid.core/src/main/java/org/nbandroid/netbeans/gradle/v2/AndroidGradleExtensionV2.java
@@ -76,6 +76,7 @@ import org.nbandroid.netbeans.gradle.v2.sdk.AndroidSdk;
 import org.nbandroid.netbeans.gradle.v2.sdk.AndroidSdkProvider;
 import org.nbandroid.netbeans.gradle.v2.template.PrivilegedTemplatesImpl;
 import org.netbeans.api.project.Project;
+import org.netbeans.gradle.nbandroid.models.AndroidNbModel;
 import org.netbeans.gradle.project.NbGradleProject;
 import org.netbeans.gradle.project.api.config.GradleArgumentQuery;
 import org.netbeans.gradle.project.api.entry.GradleProjectExtension2;
@@ -97,7 +98,7 @@ import org.openide.util.lookup.Lookups;
  *
  * @author radim
  */
-public class AndroidGradleExtensionV2 implements GradleProjectExtension2<SerializableLookup>, PropertyChangeListener {
+public class AndroidGradleExtensionV2 implements GradleProjectExtension2<AndroidNbModel>, PropertyChangeListener {
 
     private static final Logger LOG = Logger.getLogger(AndroidGradleExtensionV2.class.getName());
 
@@ -191,9 +192,9 @@ public class AndroidGradleExtensionV2 implements GradleProjectExtension2<Seriali
     }
 
     @Override
-    public void activateExtension(SerializableLookup parsedModel) {
+    public void activateExtension(AndroidNbModel parsedModel) {
         if (sdk != null && sdk.isValid()) {
-            modelsLoaded(parsedModel.lookup);
+            modelsLoaded((Lookup) parsedModel.getLookup());
         }
     }
 
