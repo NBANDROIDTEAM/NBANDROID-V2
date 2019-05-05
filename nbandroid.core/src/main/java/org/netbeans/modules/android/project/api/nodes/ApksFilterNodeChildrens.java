901de5adb1082e3b475f4b82d27f4dcd19533434
arsi-apli <arsi@arsi.sk>
2019-05-05 16:46:43 +0200

    fixed #213 Generated APKs are not shown in the project view

--- nbandroid.core/src/main/java/org/netbeans/modules/android/project/api/nodes/ApksFilterNodeChildrens.java
+++ nbandroid.core/src/main/java/org/netbeans/modules/android/project/api/nodes/ApksFilterNodeChildrens.java
@@ -18,23 +18,28 @@
  */
 package org.netbeans.modules.android.project.api.nodes;
 
+import com.android.build.OutputFile;
+import com.android.build.VariantOutput;
+import com.android.builder.model.ProjectBuildOutput;
+import com.android.builder.model.VariantBuildOutput;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import org.netbeans.api.project.Project;
-import org.openide.filesystems.FileAttributeEvent;
-import org.openide.filesystems.FileChangeListener;
-import org.openide.filesystems.FileEvent;
 import org.openide.filesystems.FileObject;
-import org.openide.filesystems.FileRenameEvent;
 import org.openide.filesystems.FileUtil;
 import org.openide.loaders.DataObject;
+import org.openide.loaders.DataObjectNotFoundException;
 import org.openide.nodes.Children;
 import org.openide.nodes.FilterNode;
 import org.openide.nodes.Node;
 import org.openide.util.Exceptions;
+import org.openide.util.Lookup;
+import org.openide.util.LookupEvent;
+import org.openide.util.LookupListener;
 import org.openide.util.RequestProcessor;
 import org.openide.util.WeakListeners;
 
@@ -42,100 +47,77 @@ import org.openide.util.WeakListeners;
  *
  * @author arsi
  */
-public class ApksFilterNodeChildrens extends Children.Keys<DataObject> implements FileChangeListener {
+public class ApksFilterNodeChildrens extends Children.Keys<DataObject> implements  LookupListener {
 
     private final Project p;
     private static final RequestProcessor RP = new RequestProcessor("Refresh APK Nodes", 1);
+    final Lookup.Result<ProjectBuildOutput> lookupResult;
 
     public ApksFilterNodeChildrens(Project p) {
         super(true);
         this.p = p;
-        List<FileObject> folders = findApks();
-
-        for (FileObject folder : folders) {
-            folder.addRecursiveListener(WeakListeners.create(FileChangeListener.class, this, folder));
-        }
+        lookupResult = p.getLookup().lookupResult(ProjectBuildOutput.class);
+        lookupResult.addLookupListener(WeakListeners.create(LookupListener.class, this, lookupResult));
     }
 
-    private List<FileObject> findApks() {
-        List<DataObject> keys = new ArrayList<>();
-        List<FileObject> folders = new ArrayList<>();
-        try {
-            FileObject release = FileUtil.createFolder(p.getProjectDirectory(), "release");
-            folders.add(release);
-            FileObject[] childrens = release.getChildren();
-            for (FileObject children : childrens) {
-                if ("apk".equals(children.getExt())) {
-                    keys.add(DataObject.find(children));
-                }
-            }
-        } catch (IOException ex) {
-            Exceptions.printStackTrace(ex);
-        }
-        try {
-            FileObject debug = FileUtil.createFolder(p.getProjectDirectory(), "debug");
-            folders.add(debug);
-            FileObject[] childrens = debug.getChildren();
-            for (FileObject children : childrens) {
-                if ("apk".equals(children.getExt())) {
-                    keys.add(DataObject.find(children));
-                }
-            }
-        } catch (IOException ex) {
-            Exceptions.printStackTrace(ex);
-        }
-        Collections.sort(keys, new Comparator<DataObject>() {
-            @Override
-            public int compare(DataObject o1, DataObject o2) {
-                return o1.getPrimaryFile().getName().compareTo(o2.getPrimaryFile().getName());
-            }
-        });
-        setKeys(keys);
-        return folders;
-    }
 
-    private void refreshFolders(FileEvent fe) {
-
-        Runnable runnable = new Runnable() {
-            public void run() {
-                findApks();
-            }
-        };
-        RP.execute(runnable);
-    }
 
     @Override
     protected Node[] createNodes(DataObject key) {
         return new Node[]{new FilterNode(key.getNodeDelegate())};
     }
 
-    @Override
-    public void fileFolderCreated(FileEvent fe) {
-        refreshFolders(fe);
-    }
 
     @Override
-    public void fileDataCreated(FileEvent fe) {
-        refreshFolders(fe);
-    }
-
-    @Override
-    public void fileChanged(FileEvent fe) {
-        refreshFolders(fe);
-    }
-
-    @Override
-    public void fileDeleted(FileEvent fe) {
-        refreshFolders(fe);
-    }
-
-    @Override
-    public void fileRenamed(FileRenameEvent fe) {
-        refreshFolders(fe);
-    }
-
-    @Override
-    public void fileAttributeChanged(FileAttributeEvent fe) {
+    public void resultChanged(LookupEvent le) {
+        List<DataObject> dobs = new ArrayList<>();
+        Collection<? extends ProjectBuildOutput> allInstances = lookupResult.allInstances();
+        if (!allInstances.isEmpty()) {
+            ProjectBuildOutput pbo = allInstances.iterator().next();
+            Collection<VariantBuildOutput> variantsBuildOutput = pbo.getVariantsBuildOutput();
+            for (VariantBuildOutput vbo : variantsBuildOutput) {
+                Collection<OutputFile> outputs = vbo.getOutputs();
+                for (OutputFile outputFile : outputs) {
+                    String outputType = outputFile.getOutputType();
+                    if (VariantOutput.MAIN.equals(outputType)) {
+                        FileObject fo = FileUtil.toFileObject(outputFile.getOutputFile());
+                        if (fo != null) {
+                            try {
+                                DataObject dob = DataObject.find(fo);
+                                if (!dobs.contains(dob)) {
+                                    dobs.add(dob);
+                                }
+                            } catch (DataObjectNotFoundException ex) {
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        try {
+            FileObject release = FileUtil.createFolder(p.getProjectDirectory(), "release");
+            FileObject[] childrens = release.getChildren();
+            for (FileObject children : childrens) {
+                if ("apk".equals(children.getExt())) {
+                    DataObject dob = DataObject.find(children);
+                    if (!dobs.contains(dob)) {
+                        dobs.add(dob);
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        Collections.sort(dobs, new Comparator<DataObject>() {
+            @Override
+            public int compare(DataObject o1, DataObject o2) {
+                return o1.getPrimaryFile().getName().compareTo(o2.getPrimaryFile().getName());
+            }
+        });
+        Runnable runnable = () -> {
+            setKeys(dobs);
+        };
+        RP.execute(runnable);
     }
 
 }
